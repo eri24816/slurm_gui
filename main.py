575c8cc702b7cf4ffb41ea4cda48577178f0e82f
eri24816 <a931eric@gmail.com>
2022-09-29 12:23:32 +0800

    forget config and last_submit_form

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_socketio import SocketIO
 from flask import request, Flask, Blueprint, flash, g, redirect, render_template,  session, url_for
 from src import socketio,create_app
+import json
 
 app = create_app()
 
@@ -11,7 +12,7 @@ def run():
     app = create_app()
     socketio.init_app(app)
     app.secret_key = __import__('os').urandom(24)
-    socketio.run(app,debug=True,port=5000,ssl_context='adhoc')
+    socketio.run(app,debug=True,port=int(json.load(open('config.json'))["port"]),ssl_context='adhoc')
 
 if __name__ == '__main__':
     run()
